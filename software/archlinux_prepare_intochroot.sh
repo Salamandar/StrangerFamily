1e86b68bafda2aa6e9905c76cb6237481afdd6bd
Félix Piédallu <felix@piedallu.me>
2017-12-20 17:48:12 +0100

    Add some for install

--- software/archlinux_prepare_intochroot.sh
+++ software/archlinux_prepare_intochroot.sh
@@ -1,9 +1,34 @@
-#!/bin/bash
+#!/bin/bash -ex
+
+# Update system
+pacman -Syuu --noconfirm
+pacman -S    --noconfirm --needed sudo git base-devel
+
+# Create non-root user
+useradd --create-home mike
+# passwd mike
+asMike() { sudo -u mike -s -- $@; }
+
+cd /tmp
+
+# Install AUR helper
+asMike git clone https://aur.archlinux.org/trizen.git
+cd trizen
+# Install missing dependencies as root
+(source PKGBUILD && pacman -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}")
+# makepkg cannot be run as root
+asMike makepkg
+pacman -U --noconfirm ./trizen-*.pkg.tar.*
 
 
-echo "I am inside chroot!"
-ls
-pwd
-/bin/bash
 
-# uname -a
+# Install led strip library
+pacman -S --noconfirm --needed python python-setuptools scons swig
+asMike git clone https://github.com/jgarff/rpi_ws281x
+cd rpi_ws281x
+asMike scons
+cd python
+python setup.py install
+
+
+bash
