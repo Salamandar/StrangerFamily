497ea9edaf53b17518c5a7e04b028ef1ca93d480
Félix Piédallu <felix@piedallu.me>
2017-12-21 10:30:43 +0100

    Split script into functions.

--- software/archlinux_prepare_intochroot.sh
+++ software/archlinux_prepare_intochroot.sh
@@ -1,34 +1,59 @@
 #!/bin/bash -ex
 
-# Update system
-pacman -Syuu --noconfirm
-pacman -S    --noconfirm --needed sudo git base-devel
+updateSystem() {
+    pacman -Syuu --noconfirm
+    pacman -S    --noconfirm --needed sudo git base-devel
+}
 
 # Create non-root user
-useradd --create-home mike
-# passwd mike
+createUser() {
+    useradd --create-home mike
+    # passwd mike
+}
 asMike() { sudo -u mike -s -- $@; }
 
-cd /tmp
-
 # Install AUR helper
-asMike git clone https://aur.archlinux.org/trizen.git
-cd trizen
-# Install missing dependencies as root
-(source PKGBUILD && pacman -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}")
-# makepkg cannot be run as root
-asMike makepkg
-pacman -U --noconfirm ./trizen-*.pkg.tar.*
+installTrizen() {
+    cd /tmp
+    asMike git clone https://aur.archlinux.org/trizen.git
+    cd trizen
+    # Install missing dependencies as root
+    (source PKGBUILD && pacman -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}")
+    # makepkg cannot be run as root
+    asMike makepkg
+    pacman -U --noconfirm ./trizen-*.pkg.tar.*
+}
+
+
+installRpiWs281x() {
+    pacman -S --noconfirm --needed python python-setuptools scons swig
+    asMike git clone https://github.com/jgarff/rpi_ws281x
+    cd rpi_ws281x
+    asMike scons
+    cd python
+    python setup.py install
+
+}
+
+installI2c() {
+    pacman -S --noconfirm --needed i2c-tools lm_sensors
+
+    # Reboot-persistent
+    echo "i2c-bcm2708"  >> /etc/modules-load.d/raspberrypi.conf
+    echo "i2c-dev"      >> /etc/modules-load.d/raspberrypi.conf
+    # For now:
+    modprobe "i2c-bcm2708"
+    modprobe "i2c-dev"
+}
 
 
 
-# Install led strip library
-pacman -S --noconfirm --needed python python-setuptools scons swig
-asMike git clone https://github.com/jgarff/rpi_ws281x
-cd rpi_ws281x
-asMike scons
-cd python
-python setup.py install
 
+updateSystem
+createUser
+
+installTrizen
+installRpiWs281x
+installI2c
 
 bash
