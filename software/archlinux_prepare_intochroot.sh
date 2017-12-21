64cd592412e560c5818b442f563e0982499da225
Félix Piédallu <felix@piedallu.me>
2017-12-21 11:46:26 +0100

    Use pushd, add installCode

--- software/archlinux_prepare_intochroot.sh
+++ software/archlinux_prepare_intochroot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
+ScriptDir="$(dirname $(readlink -f $0))"
 
 updateSystem() {
     pacman -Syuu --noconfirm
@@ -14,25 +15,28 @@ asMike() { sudo -u mike -s -- $@; }
 
 # Install AUR helper
 installTrizen() {
-    cd /tmp
-    asMike git clone https://aur.archlinux.org/trizen.git
-    cd trizen
+    if pacman -Qi trizen &>/dev/null; then return; fi
+
+    asMike git clone https://aur.archlinux.org/trizen.git /tmp/trizen
+    pushd /tmp/trizen
     # Install missing dependencies as root
     (source PKGBUILD && pacman -S --noconfirm --needed --asdeps "${makedepends[@]}" "${depends[@]}")
     # makepkg cannot be run as root
     asMike makepkg
     pacman -U --noconfirm ./trizen-*.pkg.tar.*
+    popd
 }
 
 
 installRpiWs281x() {
     pacman -S --noconfirm --needed python python-setuptools scons swig
-    asMike git clone https://github.com/jgarff/rpi_ws281x
-    cd rpi_ws281x
+    asMike git clone https://github.com/jgarff/rpi_ws281x /tmp/rpi_ws281x
+    pushd /tmp/rpi_ws281x
     asMike scons
-    cd python
+    pushd python
     python setup.py install
-
+    popd
+    popd
 }
 
 installI2c() {
@@ -41,13 +45,28 @@ installI2c() {
     # Reboot-persistent
     echo "i2c-bcm2708"  >> /etc/modules-load.d/raspberrypi.conf
     echo "i2c-dev"      >> /etc/modules-load.d/raspberrypi.conf
-    # For now:
-    modprobe "i2c-bcm2708"
-    modprobe "i2c-dev"
+    # For now: (useless inside chroot)
+    # modprobe "i2c-bcm2708"
+    # modprobe "i2c-dev"
 }
 
 
+installCode() {
+    # Copy code to the correct location
+    cp -Tr "${ScriptDir}/StrangerFamily" /home/mike/StrangerFamily
 
+    # Setup boot service
+    cp "${ScriptDir}/stranger-family.service" \
+              /usr/lib/systemd/system/stranger-family.service
+    chmod 644 /usr/lib/systemd/system/stranger-family.service
+    # Useless inside chroot:
+    # systemctl daemon-reload
+    systemctl enable stranger-family
+    # Useless inside chroot:
+    # systemctl status stranger-family # Should be enabled, stopped
+}
+
+echo "Inside the chroot !"
 
 updateSystem
 createUser
@@ -55,5 +74,8 @@ createUser
 installTrizen
 installRpiWs281x
 installI2c
+
+installCode
+
 
 bash
