10b5ad46751fe10449822eee50df6c5ab7992658
Félix Piédallu <felix@piedallu.me>
2017-12-21 13:25:01 +0100

    Python3 : fix indent, print()

--- software/StrangerFamily/letterLights.py
+++ software/StrangerFamily/letterLights.py
@@ -1,3 +1,5 @@
+#!/bin/env python3
+
 # NeoPixel library strandtest example
 # Author: Tony DiCola (tony@tonydicola.com)
 #
@@ -14,15 +16,15 @@ import argparse
 import signal
 import sys
 def signal_handler(signal, frame):
-        colorWipe(strip, Color(0,0,0))
-        sys.exit(0)
+    colorWipe(strip, Color(0,0,0))
+    sys.exit(0)
 
 def opt_parse():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-c', action='store_true', help='clear the display on exit')
-        args = parser.parse_args()
-        if args.c:
-                signal.signal(signal.SIGINT, signal_handler)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', action='store_true', help='clear the display on exit')
+    args = parser.parse_args()
+    if args.c:
+        signal.signal(signal.SIGINT, signal_handler)
 
 # LED strip configuration:
 LED_COUNT      = 38      # Number of LED pixels.
@@ -83,7 +85,7 @@ def getLetterPositions(letter):
     tableLetterPosition = letters.keys().index(letter)
     ledLetterPosition = 0
 
-    print tableLetterPosition;
+    print(tableLetterPosition);
 
     for key in letters:
         if key != letter:
@@ -93,8 +95,8 @@ def getLetterPositions(letter):
 
     letterPositions[0] = ledLetterPosition
     letterPositions[1] = letters[letter]
-    print letter
-    print letterPositions
+    print(letter)
+    print(letterPositions)
     return letterPositions
 
 def lightning(strip, letterPositions):
@@ -111,7 +113,7 @@ def lightning(strip, letterPositions):
     green = random.randint(0, 255)
     blue = random.randint(0, 255)
 
-# i-eme cycle
+    # i-eme cycle
     while timeLeft>0:
         wait_ms = random.randint(0, timeLeft)
         if i%2==0:
@@ -125,7 +127,7 @@ def lightning(strip, letterPositions):
         time.sleep(wait_ms/1000.0)
         timeLeft = timeLeft-wait_ms
         i += 1
-	shutoffLights(strip)
+    shutoffLights(strip)
 
 
 def shutoffLights(strip):
@@ -134,32 +136,32 @@ def shutoffLights(strip):
     strip.show()
 
 def getSentence():
-lineNb = 0
-with open('sentences.txt', 'r') as f:
-    num_lines = sum(1 for line in f)
-    lineNb = random.randint(0, num_lines-1)
-with open('sentences.txt', 'r') as f:
-    for i, line in enumerate(f):
-        if i == lineNb:
-            return line
+    lineNb = 0
+    with open('sentences.txt', 'r') as f:
+        num_lines = sum(1 for line in f)
+        lineNb = random.randint(0, num_lines-1)
+    with open('sentences.txt', 'r') as f:
+        for i, line in enumerate(f):
+            if i == lineNb:
+                return line
 
 
 
 
 # Main program logic follows:
 if __name__ == '__main__':
-        # Process arguments
-        opt_parse()
+    # Process arguments
+    opt_parse()
 
-	# Create NeoPixel object with appropriate configuration.
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-	# Intialize the library (must be called once before other functions).
-	strip.begin()
-	shutoffLights(strip)
+    # Create NeoPixel object with appropriate configuration.
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
+    shutoffLights(strip)
 
 
     string = getSentence() #"Friends dont lie"
-	sequenceOfAlphabets = list(string.upper())
-	for letter in sequenceOfAlphabets:
-	    lightning(strip, getLetterPositions(letter))
-	    time.sleep(0.3)
+    sequenceOfAlphabets = list(string.upper())
+    for letter in sequenceOfAlphabets:
+        lightning(strip, getLetterPositions(letter))
+        time.sleep(0.3)
